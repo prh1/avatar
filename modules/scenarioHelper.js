69c646a14da6ee71a74f4b8b8359e23b2daf3248
Paul Hodgson <prhodgson1@gmail.com>
2019-07-23 08:34:17 +0100

    Add missing modules directory

---
+++ modules/scenarioHelper.js
@@ -0,0 +1,64 @@
+﻿var q = require('q');
+var crypto = require('crypto');
+
+module.exports = function (config, logger) {
+    var ttsHelper = new (require('./ttsHelper.js'))(config.voice, config, logger);
+
+    var self = this;
+
+    self.prepareScene = function(scene) {
+        logger.trace("Preparing scene");
+        var deferred = q.defer();
+
+        var lines = [];
+
+        for (var key in scene) {
+            for (var i = 0; i < scene[key].length; i++) {
+                var line = scene[key][i].text;
+                scene[key][i].speechid = crypto.createHash("md5").update(line).digest("hex");
+                lines.push(ttsHelper.getSpeech.bind(null, line));
+            }
+        }
+        var chain = q();
+        lines.forEach(function (f) {
+            chain = chain.then(f);
+        });
+
+        chain.then(function (d) {
+            deferred.resolve(true);
+        });
+
+        return deferred.promise;
+    }
+
+    self.prepareScenario = function (scenario) {
+        var script = scenario.script;
+        var scenes = scenario.scenes;
+        console.log('Preparing', JSON.stringify(script), JSON.stringify(scenes));
+        scenario.position = {
+            scene: script[0],
+            block: "start",
+            offset: 0
+        };
+
+        var deferred = q.defer();
+        var promises = [];
+
+        for (var i = 0; i < script.length; i++) {
+            console.log(i, scenes[script[i]]);
+            promises.push(self.prepareScene.bind(null, scenes[script[i]]));
+        }
+
+        var chain = q();
+        promises.forEach(function (f) {
+            chain = chain.then(f)
+        });
+        chain.then(function (d) {
+            deferred.resolve();
+        });
+
+        return deferred.promise;
+    }
+
+    return self;
+}
---
+++ modules/ttsHelper.js
@@ -0,0 +1,77 @@
+﻿const AWS = require('aws-sdk');
+
+
+const q = require('q');
+const crypto = require('crypto');
+const fs = require('fs');
+
+var TTS = function (defaultVoice, config, logger) {
+
+    const Polly = new AWS.Polly({
+        signatureVersion: 'v4',
+        region: config.region, 
+        accessKeyId: config.accessKeyId,
+        secretAccessKey: config.secretAccessKey
+    });
+    
+
+    logger.trace("Building TTS helper");
+
+    var self = this;
+    self.defaultVoice = defaultVoice;
+
+    self.getSpeech = function(text) {
+        var deferred = q.defer();
+
+        voice = self.defaultVoice;
+
+        var fileId = crypto.createHash("md5").update(text).digest("hex");
+        var path = config.speechPath;
+
+        var fileName = path + voice + fileId;
+
+        if (fs.existsSync(fileName + ".txt")) {
+            logger.trace("Speech already exists: ", text);
+            deferred.resolve(true); // Already have the speech file
+            return deferred.promise;
+        }
+        logger.trace("Synthesizing speech for: ", text);
+
+        var params = {
+            'Text': text,
+            'OutputFormat': 'mp3',
+            'VoiceId': voice
+        }
+
+
+        Polly.synthesizeSpeech(params, (err, data) => {
+            if (err) {
+                console.log(err.code);
+            } else if (data) {
+                if (data.AudioStream instanceof Buffer) {
+                    fs.writeFile(fileName + ".mp3", data.AudioStream, function (err) {
+                        logger.trace("The mp3 file was saved");
+                        if (err) {
+                            return console.log(err)
+                        };
+                        params.OutputFormat = "json";
+                        params.SpeechMarkTypes = ['word', 'viseme'];
+                        Polly.synthesizeSpeech(params, (err, data) => {
+                            if (err) {
+                                console.log(err.code)
+                            } else {
+                                fs.writeFile(fileName + ".json", data.AudioStream, function () { });
+                                fs.writeFileSync(fileName + ".txt", text);
+                           }
+                            deferred.resolve(true);
+                        });
+                    })
+                }
+            }
+        });
+
+        return deferred.promise;
+    }
+}
+
+module.exports = TTS;
